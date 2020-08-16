0f34c9a66f23157ea7bed2b3a3689fef00bb557f
lfq <731868691@qq.com>
2020-08-16 23:42:27 +0800

    观察者模式实现

--- js/设计模式/发布订阅模式/index.js
+++ js/设计模式/发布订阅模式/index.js
@@ -1,6 +1,7 @@
      class Subscribe {
         constructor(){
             this.callbacks=[]
+            this.results=[]
         }
         //订阅
         on(cb){
@@ -8,28 +9,30 @@
            if(!hasOwn) this.callbacks.push(cb)
         }
         //发布
-        emit(){
+        emit(data){
+            this.results.push(data)
             this.callbacks.forEach((cb)=>{
-                cb()
+                cb(this.results)
             })
         }
     }
 
 let s=new Subscribe()
-function f1(){
-    console.log('1')
+function f1(res){
+    console.log(res)
 }
-function f2(){
-    console.log('2')
+function f2(res){
+    console.log(res)
 }
-function f3(){
-    console.log('3')
+function f3(res){
+    console.log(res)
 }
 s.on(f1)
 s.on(f1)
 s.on(f2)
 s.on(f3)
-s.emit()
+s.emit('今天发了一件事情')
+s.emit('今天又发了一件事情')
 
 
 
---
+++ js/设计模式/观察者模式/index.js
@@ -0,0 +1,35 @@
+//被观察者
+class Subject {
+    constructor(name, height) {
+        this.observes = []
+        this.state = "我今天很开心"
+        this.name = name
+        this.height = height
+    }
+    //接收所有的观察者
+    attach(ob) {
+        this.observes.push(ob)
+    }
+    //自身状态更改，通知观察者
+    setState(newHeight) {
+        this.observes.forEach((o) => { o.update(this.height, newHeight) })
+    }
+}
+//观察者
+class Observe {
+    //被观察者状态更新时，触发
+    update(age, newAge) {
+        if (newAge > age) {
+            console.log('我家孩子长高了')
+        } else {
+            console.log('我家孩子怎么变矮了')
+        }
+    }
+}
+let baby = new Subject('baby', 155)
+let ob1 = new Observe()
+let ob2 = new Observe()
+baby.attach(ob1)
+baby.attach(ob2)
+
+baby.setState(154)
